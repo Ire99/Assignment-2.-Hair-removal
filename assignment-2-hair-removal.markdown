79e78cf67cc815bf511472c368c800b07b0625b5
Ire99 <99166822+Ire99@users.noreply.github.com>
2022-03-08 15:54:19 -0800

    Add files via upload

---
+++ assignment-2-hair-removal.markdown
@@ -0,0 +1,7 @@
+Assignment 2. Hair removal
+--------------------------
+
+
+A [Pen](https://codepen.io/ire99/pen/gOXymLw) by [Ire99](https://codepen.io/ire99) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/license/pen/gOXymLw).
---
+++ script.js
@@ -0,0 +1,63 @@
+var hair1 = document.querySelector(".hair1");
+hair1.addEventListener("click", function (event) {
+  hair1.style.display = "none";
+});
+var hair2 = document.querySelector(".hair2");
+hair2.addEventListener("click", function (event) {
+  hair2.style.display = "none";
+});
+var hair3 = document.querySelector(".hair3");
+hair3.addEventListener("click", function (event) {
+  hair3.style.display = "none";
+});
+var hair4 = document.querySelector(".hair4");
+hair4.addEventListener("click", function (event) {
+  hair4.style.display = "none";
+});
+var hair5 = document.querySelector(".hair5");
+hair5.addEventListener("click", function (event) {
+  hair5.style.display = "none";
+});
+var hair6 = document.querySelector(".hair6");
+hair6.addEventListener("click", function (event) {
+  hair6.style.display = "none";
+});
+var hair7 = document.querySelector(".hair7");
+hair7.addEventListener("click", function (event) {
+  hair7.style.display = "none";
+});
+
+var hair8 = document.querySelector(".hair8");
+hair8.addEventListener("click", function (event) {
+  hair8.style.display = "none";
+});
+
+var hair9 = document.querySelector(".hair9");
+hair9.addEventListener("click", function (event) {
+  hair9.style.display = "none";
+});
+
+var hair10 = document.querySelector(".hair10");
+hair10.addEventListener("click", function (event) {
+  hair10.style.display = "none";
+});
+
+var hair11 = document.querySelector(".hair11");
+hair11.addEventListener("click", function (event) {
+  hair11.document.getElementById("hide5").style.display = "none";
+});
+
+var hair12 = document.querySelector(".hair12");
+hair12.addEventListener("click", function (event) {
+  hair12.style.display = "none";
+});
+
+var hair13 = document.querySelector(".hair13");
+hair13.addEventListener("click", function (event) {
+hair13.style.display = "none";
+});
+
+var hair14 = document.querySelector(".hair14");
+hair14.addEventListener("click", function (event) {
+  hair14.style.display = "none";
+});
